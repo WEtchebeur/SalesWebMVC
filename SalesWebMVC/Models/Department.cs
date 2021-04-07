98baca8bb9d6efd5b2bd54db20750eb0576abb7c
William Etchebeur <williametchebeur@gmail.com>
2021-04-07 14:16:14 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,6 +9,7 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department()
         {
@@ -18,6 +19,16 @@ namespace SalesWebMVC.Models
         {
             Id = id;
             Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
     }
 }
